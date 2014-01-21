c28adc29aed21a82f88d9bccbdf22d2a762d0c40
Andrew Thompson <athompson@azavea.com>
2014-01-21 15:48:58 -0500

    remove references to dict notation

--- cicero_examples.py
+++ cicero_examples.py
@@ -1,7 +1,7 @@
-from cicero_rest_connection import *
+from cicero import *
 
-USERNAME = "cicero api username goes here"
-PASSWORD = "cicero api password goes here"
+USERNAME = "example" #put your cicero API username here
+PASSWORD = "example" #put your cicero API password here
 
 # Example Usage:
 
@@ -22,22 +22,11 @@ print philly_officials.response.results.candidates[0].officials[0].last_name
 #by Cicero in alphabetical order by last_name) for Philadelphia, currently
 #"Biden" for US Vice President Joe Biden.
 
-#OR, access official info using ".__dict__" attribute and standard python
-#dictionary "bracket" notation
-
-print philly_officials.__dict__['response']['results']['candidates'][0]['officials'][1]['last_name']
-#will print "Biden" same as above
-
-#The ".__dict__" attribute may be used at any point. For instance,
-
-print philly_officials.response.results.candidates[0].__dict__['officials'][1]['last_name']
-#"Biden", same as both prints above
-
 #You may also initialize the class without storing an instance in a
-#variable, and access methods and even the API responses with either dot
-#notation OR standard python dictionary bracket notation.
+#variable, and access methods and even the API responses with dot
+#notation.
 
-#For example (each enclosed in parentheses for newlines/readability),
+#For example (enclosed in parentheses for newlines/readability),
 
 print (CiceroRestConnection(USERNAME, PASSWORD)
         .get_official(search_loc="340 N 12th St, Philadelphia, PA USA",
@@ -47,16 +36,5 @@ print (CiceroRestConnection(USERNAME, PASSWORD)
         .candidates[0]
         .officials[0]
         .last_name )
-        
-print (CiceroRestConnection(USERNAME, PASSWORD)
-        .get_official(search_loc="340 N 12th St, Philadelphia, PA USA",
-                      district_type="STATE_LOWER")
-        .__dict__
-        ['response']
-        ['results']
-        ['candidates'][0]
-        ['officials'][0]
-        ['last_name'])
 
-#Each should print "O'Brien", the last name of Azavea's current PA state
-#representative.
+#should print "O'Brien", the last name of Azavea's current PA state representative.
